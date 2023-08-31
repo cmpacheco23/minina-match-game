92b3d7f41e0e91b40df001ea934ad2faba293173
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-08-31 19:59:21 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log('minina check')
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+  /*------------------------ Cached Element References ------------------------*/
+
+// Step 2 - Store cached element references.
+
+  /*----------------------------- Event Listeners -----------------------------*/
+
+
+  /*-------------------------------- Functions --------------------------------*/
+// Step 3 - Upon loading, the game state should be initialized
